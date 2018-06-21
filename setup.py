7f5a8a2fe208d67b05e7bf1191b7637b7f88c9b7
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2018-06-22 01:37:58 +0300

    add version

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
 from setuptools import setup
+import re
+
+
+def get_version():
+    lines = open('gpu-batch.sub', 'rt').readlines()
+    version_regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    for line in lines:
+        mo = re.search(version_regex, line, re.M)
+        if mo:
+            return mo.group(1)
+    raise RuntimeError('Unable to find version in %s.' % ('gpu-batch.sub',))
+
 
 if __name__ == '__main__':
     try:
@@ -10,6 +22,7 @@ if __name__ == '__main__':
         name='gpu-batch',
         description='Batch bsub launcher',
         long_description=long_desc,
+        version=get_version(),
         author='Maxim Kochurov <maxim.v.kochurov@gmail.com>',
         python_requires='>=3.5',
         scripts=['gpu-batch.sub'],
